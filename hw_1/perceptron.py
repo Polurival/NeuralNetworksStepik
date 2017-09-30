a85e11dc2855a9d5c35516bdf64e4733173e8f26
Polurival <polurival@gmail.com>
2017-09-30 22:54:39 +0300

    Перцептрон практика 1

---
+++ hw_1/perceptron.py
@@ -0,0 +1,310 @@
+"""
+https://github.com/stacymiller/stepic_neural_networks_public/blob/master/HW_1/Hw_1_student_version.ipynb
+"""
+
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as p3
+import numpy as np
+import random
+import time
+
+from functools import partial
+from ipywidgets import interact, RadioButtons, IntSlider, FloatSlider, Dropdown, BoundedFloatText
+from numpy.linalg import norm
+
+random.seed(42)  # начальное состояние генератора случайных чисел, чтобы можно было воспроизводить результаты.
+
+# %matplotlib inline
+data = np.loadtxt("data.csv", delimiter=",")
+
+
+pears = data[:, 2] == 1
+apples = np.logical_not(pears)
+plt.scatter(data[apples][:, 0], data[apples][:, 1], color = "red")
+plt.scatter(data[pears][:, 0], data[pears][:, 1], color = "green")
+plt.xlabel("yellowness")
+plt.ylabel("symmetry")
+plt.show()
+
+
+
+class Perceptron:
+    def __init__(self, w, b):
+        """
+        Инициализируем наш объект - перцептрон.
+        w - вектор весов размера (m, 1), где m - количество переменных
+        b - число
+        """
+
+        self.w = w
+        self.b = b
+
+    def forward_pass(self, single_input):
+        """
+        Метод рассчитывает ответ перцептрона при предъявлении одного примера
+        single_input - вектор примера размера (m, 1).
+        Метод возвращает число (0 или 1) или boolean (True/False)
+        """
+
+        result = 0
+        for i in range(0, len(self.w)):
+            result += self.w[i] * single_input[i]
+        result += self.b
+
+        if result > 0:
+            return 1
+        else:
+            return 0
+
+    def vectorized_forward_pass(self, input_matrix):
+        """
+        Метод рассчитывает ответ перцептрона при предъявлении набора примеров
+        input_matrix - матрица примеров размера (n, m), каждая строка - отдельный пример,
+        n - количество примеров, m - количество переменных
+        Возвращает вертикальный вектор размера (n, 1) с ответами перцептрона
+        (элементы вектора - boolean или целые числа (0 или 1))
+        """
+
+        # перемножаем матрицу входных данных на матрицу весов и прибавляем смещение
+        sum_vector_matrix = input_matrix.dot(self.w) + self.b
+        # активационное правило перцептрона:
+        # если для i-го примера сумма произведений весов на входные параметры признаков + смещение > 0,
+        # то алгоритм угадал с ответом и в активационную матрицу записывается 1, иначе 0.
+        activation_vector_matrix = sum_vector_matrix > 0
+        return activation_vector_matrix
+
+    def train_on_single_example(self, example, y):
+        """
+        принимает вектор активации входов example формы (m, 1)
+        и правильный ответ для него (число 0 или 1 или boolean),
+        обновляет значения весов перцептрона в соответствии с этим примером
+        и возвращает размер ошибки, которая случилась на этом примере до изменения весов (0 или 1)
+        (на её основании мы потом построим интересный график)
+        """
+
+        y_caps = self.vectorized_forward_pass(example.T)
+        # вычисляем ошибку (она может быть 0 или 1)
+        error = y - y_caps[0, 0]
+        # обновляем вектор весов
+        self.w += error * example
+        # обновляем смещение
+        self.b += error
+
+        return abs(error)
+
+    def train_until_convergence(self, input_matrix, y, max_steps=1e8):
+        """
+        input_matrix - матрица входов размера (n, m),
+        y - вектор правильных ответов размера (n, 1) (y[i] - правильный ответ на пример input_matrix[i]),
+        max_steps - максимальное количество шагов.
+        Применяем train_on_single_example, пока не перестанем ошибаться или до умопомрачения.
+        Константа max_steps - наше понимание того, что считать умопомрачением.
+        """
+        i = 0
+        errors = 1
+        while errors and i < max_steps:
+            i += 1
+            errors = 0
+            for example, answer in zip(input_matrix, y):
+                example = example.reshape((example.size, 1))
+                error = self.train_on_single_example(example, answer)
+                errors += int(error)  # int(True) = 1, int(False) = 0, так что можно не делать if
+
+
+# perc = Perceptron(np.array([[1.6], [-3.1]]), 1)
+# perc.train_on_single_example(np.array([[1], [0.3]]), 0)
+
+def create_perceptron(m):
+    """Создаём перцептрон со случайными весами и m входами"""
+    w = np.random.random((m, 1))
+    return Perceptron(w, 1)
+
+def plot_line(coefs):
+    """
+    рисует разделяющую прямую, соответствующую весам, переданным в coefs = (weights, bias),
+    где weights - ndarray формы (2, 1), bias - число
+    """
+    w, bias = coefs
+    a, b = - w[0][0] / w[1][0], - bias / w[1][0]
+    xx = np.linspace(*plt.xlim())
+    line.set_data(xx, a * xx + b)
+
+
+def step_by_step_weights(p, input_matrix, y, max_steps=1e6):
+    """
+    обучает перцептрон последовательно на каждой строчке входных данных,
+    возвращает обновлённые веса при каждом их изменении
+    p - объект класса Perceptron
+    """
+    i = 0
+    errors = 1
+    while errors and i < max_steps:
+        i += 1
+        errors = 0
+        for example, answer in zip(input_matrix, y):
+            example = example.reshape((example.size, 1))
+
+            error = p.train_on_single_example(example, answer)
+            errors += error  # здесь мы упадём, если вы забыли вернуть размер ошибки из train_on_single_example
+            if error:  # будем обновлять положение линии только тогда, когда она изменила своё положение
+                yield p.w, p.b
+
+    for _ in range(20): yield p.w, p.b
+
+
+# %matplotlib nbagg
+np.random.seed(1)
+fig = plt.figure()
+plt.scatter(data[apples][:, 0], data[apples][:, 1], color = "red", marker=".", label="Apples")
+plt.scatter(data[pears][:, 0], data[pears][:, 1], color = "green", marker=".", label="Pears")
+plt.xlabel("yellowness")
+plt.ylabel("symmetry")
+line, = plt.plot([], [], color="black", linewidth=2)  # создаём линию, которая будет показывать границу разделения
+
+from matplotlib.animation import FuncAnimation
+
+perceptron_for_weights_line = create_perceptron(2)  # создаём перцептрон нужной размерности со случайными весами
+
+from functools import partial
+weights_ani = partial(
+    step_by_step_weights, p=perceptron_for_weights_line, input_matrix=data[:, :-1], y=data[:, -1][:,np.newaxis]
+)  # про partial почитайте на https://docs.python.org/3/library/functools.html#functools.partial
+
+ani = FuncAnimation(fig, func=plot_line, frames=weights_ani, blit=False, interval=10, repeat=True)
+# если Jupyter не показывает вам анимацию - раскомментируйте строчку ниже и посмотрите видео
+ani.save("perceptron_seeking_for_solution.mp4", fps=15)
+plt.show()
+
+## Не забудьте остановить генерацию новых картинок, прежде чем идти дальше (кнопка "выключить" в правом верхнем углу графика)
+
+
+def step_by_step_errors(p, input_matrix, y, max_steps=1e6):
+    """
+    обучает перцептрон последовательно на каждой строчке входных данных,
+    на каждом шаге обучения запоминает количество неправильно классифицированных примеров
+    и возвращает список из этих количеств
+    """
+
+    def count_errors():
+        return np.abs(p.vectorized_forward_pass(input_matrix).astype(np.int) - y).sum()
+
+    errors_list = [count_errors()]
+    i = 0
+    errors = 1
+    while errors and i < max_steps:
+        i += 1
+        errors = 0
+        for example, answer in zip(input_matrix, y):
+            example = example.reshape((example.size, 1))
+
+            error = p.train_on_single_example(example, answer)
+            errors += error
+            errors_list.append(count_errors())
+    return errors_list
+
+
+perceptron_for_misclassification = create_perceptron(2)
+errors_list = step_by_step_errors(perceptron_for_misclassification, input_matrix=data[:, :-1],
+                                  y=data[:, -1][:, np.newaxis])
+plt.plot(errors_list);
+plt.ylabel("Number of errors")
+plt.xlabel("Algorithm step number");
+plt.show()
+
+
+def get_vector(p):
+    """возвращает вектор из всех весов перцептрона, включая смещение"""
+    v = np.array(list(p.w.ravel()) + [p.b])
+    return v
+
+
+def step_by_step_distances(p, ideal, input_matrix, y, max_steps=1e6):
+    """обучает перцептрон p и записывает каждое изменение расстояния от текущих весов до ideal"""
+    distances = [norm(get_vector(p) - ideal)]
+    i = 0
+    errors = 1
+    while errors and i < max_steps:
+        i += 1
+        errors = 0
+        for example, answer in zip(input_matrix, y):
+            example = example.reshape((example.size, 1))
+
+            error = p.train_on_single_example(example, answer)
+            errors += error
+            if error:
+                distances.append(norm(get_vector(p) - ideal))
+    return distances
+
+
+# %matplotlib inline
+
+np.random.seed(42)
+init_weights = np.random.random_sample(3)
+w, b = init_weights[:-1].reshape((2, 1)), init_weights[-1]
+ideal_p = Perceptron(w.copy(), b.copy())
+ideal_p.train_until_convergence(data[:, :-1], data[:, -1][:,np.newaxis])
+ideal_weights = get_vector(ideal_p)
+
+new_p = Perceptron(w.copy(), b.copy())
+distances = step_by_step_distances(new_p, ideal_weights, data[:, :-1], data[:, -1][:,np.newaxis])
+
+plt.xlabel("Number of weight updates")
+plt.ylabel("Distance between good and current weights")
+plt.plot(distances)
+plt.show()
+
+
+# тестирование
+
+def test_v_f_p(n, m):
+    """
+    Расчитывает для перцептрона с m входами
+    с помощью методов forward_pass и vectorized_forward_pass
+    n ответов перцептрона на случайных данных.
+    Возвращает время, затраченное vectorized_forward_pass и forward_pass
+    на эти расчёты.
+    """
+
+    p = create_perceptron(m)
+    input_m = np.random.random_sample((n, m))
+
+    start = time.clock()
+    vec = p.vectorized_forward_pass(input_m)
+    end = time.clock()
+    vector_time = end - start
+
+    start = time.clock()
+    for i in range(0, n):
+        p.forward_pass(input_m[i])
+    end = time.clock()
+    plain_time = end - start
+
+    return [vector_time, plain_time]
+
+
+def mean_execution_time(n, m, trials=100):
+    """среднее время выполнения forward_pass и vectorized_forward_pass за trials испытаний"""
+
+    return np.array([test_v_f_p(n, m) for _ in range(trials)]).mean(axis=0)
+
+
+def plot_mean_execution_time(n, m):
+    """рисует графики среднего времени выполнения forward_pass и vectorized_forward_pass"""
+
+    mean_vectorized, mean_plain = mean_execution_time(int(n), int(m))
+    p1 = plt.bar([0], mean_vectorized, color='g')
+    p2 = plt.bar([1], mean_plain, color='r')
+
+    plt.ylabel("Time spent")
+    plt.yticks(np.arange(0, mean_plain))
+
+    plt.xticks(range(0, 1))
+    plt.legend(("vectorized", "non - vectorized"))
+
+    plt.show()
+
+# TypeError: test_v_f_p() missing 2 required positional arguments: 'n' and 'm'   ?????
+interact(plot_mean_execution_time,
+         n=RadioButtons(options=["1", "10", "100"]),
+         m=RadioButtons(options=["1", "10", "100"], separator=" "))
+
